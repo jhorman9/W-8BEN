fc6d8529a45820bfc4d94c94751241c418dc9ada
Jhorman Nieto P <jhosmita9@gmail.com>
2024-10-03 13:38:40 -0500

    agregando formulario de pasos

---
+++ form/script.js
@@ -0,0 +1,104 @@
+const fisicalEntity = document.querySelector("#fisicalorentity");
+const country = document.querySelector("#country");
+const checkEntity = document.querySelectorAll(".check-entity input");
+const checkEntityContainer = document.querySelector(".check-entity");
+const countryEntity = document.querySelector(".country-entity");
+
+fisicalEntity.addEventListener("change", (e) => {
+    const fisicalEntityValue = e.target.value;
+    if(fisicalEntityValue && fisicalEntityValue == 'fisical'){
+        localStorage.setItem("formulario", "W-8BEN");
+        console.log(fisicalEntityValue);
+    }else{
+        localStorage.removeItem("formulario");
+    }
+    if (fisicalEntityValue == 'entity') {
+        checkEntityContainer.style.display = "block"; 
+        countryEntity.style.display = "flex";   
+    }else{
+        checkEntityContainer.style.display = "none";
+        countryEntity.style.display = "none";   
+    }
+});
+
+let lastChecked = null;
+checkEntity && (
+    checkEntity.forEach(check => {
+        check.addEventListener("change", (e) => {
+            if (e.target.checked) {
+                checkEntity.forEach(otherCheck => {
+                    if (otherCheck !== e.target) {
+                        otherCheck.checked = false;
+                    }
+                });
+                lastChecked = e.target;
+            } else {
+                e.target.checked = true;
+            };
+
+            console.log(lastChecked.id)
+
+            if(lastChecked.id == 'financial-institution' || lastChecked.id == 'patrimonial-entity'){
+                return(
+                    Swal.fire({
+                        title: 'Información importante',
+                        html: `Por las particularidades de su situación, su caso no puede ser cubierto por las funciones de esta plataforma. Le sugerimos contactarnos directamente a <strong>info@giin.tax</strong> para brindarle asesoría personalizada en el llenado de sus formularios. Tenga en cuenta que para este servicio se aplicarán honorarios profesionales. Estaremos encantados de atenderle.`,
+                        icon: 'info',
+                        confirmButtonText: 'Ok'
+                    })
+                )
+            }
+        });
+    })
+)
+
+country && (
+    country.addEventListener("change", (e) => {
+        const countryValue = e.target.value;
+    
+        const countryEntity = {
+            "ar": ["Sociedad Anónima", "Sociedad de Responsabilidad Limitada", "Sociedad Anónima Unipersonal (S.A.U.)"],
+            "aru": ["“Naamloze Vennootschap (N.V.)", "Besloten Vennootschap (B.V.)"],
+            "bah": ["International Business Company","Limited Liability Company"],
+            "bar": ["Limited Company", "Limited Liabilty Company", "International Business Company"],
+            "bel": ["Public Limited Company", "Limited Liability Company"],
+            "ber": ["Exempted Company"],
+            "bol":["Sociedad Anónima", "Sociedad de Responsabilidad Limitada"],
+            "bra": ["Sociedade Anonima", "LTDA"],
+            "bri": ["BVI Business Company", "Limited Liability Company"],
+            "cay": ["Exempted Company", "Limited Liability Company"],
+            "chi": ["Sociedad Anónima", "Sociedad de Responsabilidad Limitada"],
+            "col": ["Sociedad Anónima", "Sociedad por Acciones Simplificada (S.A.S.)"],
+            "cos": ["Sociedad Anónima", "Sociedad de Responsabilidad Limitada"],
+            "cur": ["Naamloze Vennootschap (N.V.)", "Besloten Vennootschap (B.V.)"],
+            "ecu": ["Sociedad Anónima", "Compañía Anónima", "Sociedad de Responsabilidad Limitada"],
+            "esp": ["Sociedad Anónima", "Sociedad de Responsabilidad Limitada (S.L.)", "Sociedad Limitada Nueva Empresa (SLNE)"],
+            "sal": ["Sociedad Anónima", "Sociedad de Responsabilidad Limitada", "Sociedad Anónima de Capital Variable"],
+            "gua": ["Sociedad Anónima", "Sociedad de Responsabilidad Limitada", "Sociedad Anónima de Capital Variable"],
+            "hon": ["Sociedad Anónima", "Sociedad de Responsabilidad Limitada"],
+            "hong": ["Public Limited Company"],
+            "jam": ["Public Limited Company"],
+            "mal": ["Public Limited Company"],
+            "mex": ["Sociedad Anónima", "Sociedad Anónima de Capital Variable", "Sociedad de Responsabilidad Limitada"],
+            "nic": ["Compañía Anónima", "Sociedad de Responsabilidad Limitada", "constituida en Nicaragua"],
+            "pan": ["“Sociedad Anónima ” (S.A.) (Inc.) (Corp.)", "Sociedad de Responsabilidad Limitada"],
+            "par": ["Sociedad Anónima", "Sociedad de Responsabilidad Limitada"],
+            "per": ["Sociedad Anónima", "Sociedad de Responsabilidad Limitada"],
+            "por": ["Sociedade Anónima (S.A.)", "Sociedade por Quotas (Lda.)", "Sociedade Unipessoal por Quotas", "Sociedade por Quotas de Responsabilidade Limitada com Participação em Beneficios (Lda. com PAB)"],
+            "kitt": ["International Business Company", "Limited Liability Company"],
+            "uru": ["Sociedad Anónima", "Sociedad de Responsabilidad Limitada"],
+            "ven": ["Sociedad Anónima", "Compañía Anónima", "Sociedad de Responsabilidad Limitada"],
+            "otro": ["otro"]
+        };
+    
+        const typeEntity = document.querySelector('#typeentity');
+        const arrayEntity = countryEntity[countryValue];
+        
+        if(arrayEntity){
+            typeEntity.innerHTML = `
+            <option value="">Seleccione una opción</option>
+            ${arrayEntity.map(entity => `<option value="${entity.replace(/\s+/g, '')}">${entity}</option>`).join("")}`;
+        }
+        
+    })
+);
